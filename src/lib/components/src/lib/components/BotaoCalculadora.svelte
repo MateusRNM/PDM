b273b056a4d0cc56e293e0522ac1cef7446fd55b
MateusRNM <mateusmt676@gmail.com>
2025-06-03 19:26:43 -0400

    update

--- src/lib/components/src/lib/components/BotaoCalculadora.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    let { symbol, func, color } = $props()
-</script>
-
-<div class="text-center m-1 border rounded-2 w-100 h-100">
-    <button type="button" class={`btn w-100 h-100 ${color}`}>{symbol}</button>
-</div>
--- src/routes/calculadora/+page.svelte
+++ src/routes/calculadora/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-    import BotaoCalculadora from "$lib/components/src/lib/components/BotaoCalculadora.svelte";
     let expressao = $state("")
     
     function addChar(char){
@@ -9,41 +8,64 @@
     function clear(){
         expressao = ""
     }
+
+    function calcular(){
+
+    }
 </script>
 
-<div class="position-absolute start-50 top-50 translate-middle border border-3 border-black p-2">
-	<div class="container text-center"></div>
+<div class="position-absolute start-50 top-50 translate-middle border border-3 border-black p-1 rounded">
+	<div class="container text-center">
+        <input type="text" class="form-control" disabled={true} bind:value={expressao}>
+    </div>
 	<table>
         <tbody>
             <tr>
-                <td><BotaoCalculadora symbol="C" func={clear} color="btn-warning"/></td>
-                <td><BotaoCalculadora symbol="(" func={addChar} color="btn-secondary"/></td>
-                <td><BotaoCalculadora symbol=")" func={addChar} color="btn-secondary"/></td>
-                <td><BotaoCalculadora symbol="/" func={addChar} color="btn-secondary"/></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-warning" onclick={clear}>C</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("(")}>(</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar(")")}>)</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("/")}>/</button></td>
             </tr>
             <tr>
-                <td><BotaoCalculadora symbol="7" func={addChar} color="btn-dark"/></td>
-                <td><BotaoCalculadora symbol="8" func={addChar} color="btn-dark"/></td>
-                <td><BotaoCalculadora symbol="9" func={addChar} color="btn-dark"/></td>
-                <td><BotaoCalculadora symbol="X" func={addChar} color="btn-secondary"/></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar("7")}>7</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar("8")}>8</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar("9")}>9</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("X")}>X</button></td>
             </tr>
             <tr>
-                <td><BotaoCalculadora symbol="4" func={addChar} color="btn-dark"/></td>
-                <td><BotaoCalculadora symbol="5" func={addChar} color="btn-dark"/></td>
-                <td><BotaoCalculadora symbol="6" func={addChar} color="btn-dark"/></td>
-                <td><BotaoCalculadora symbol="-" func={addChar} color="btn-secondary"/></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar("4")}>4</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar("5")}>5</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar("6")}>6</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("-")}>-</button></td>
             </tr>
             <tr>
-                <td><BotaoCalculadora symbol="1" func={addChar} color="btn-dark"/></td>
-                <td><BotaoCalculadora symbol="2" func={addChar} color="btn-dark"/></td>
-                <td><BotaoCalculadora symbol="3" func={addChar} color="btn-dark"/></td>
-                <td style="height: 0;" rowspan="2"><BotaoCalculadora symbol="+" color="btn-secondary"/></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar("1")}>1</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar("2")}>2</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar("3")}>3</button></td>
+                <td style="height: 0;" rowspan="2"><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("+")}>+</button></td>
             </tr>
             <tr>
-                <td><BotaoCalculadora symbol="0" func={addChar} color="btn-dark"/></td>
-                <td><BotaoCalculadora symbol="." func={addChar} color="btn-dark"/></td>
-                <td><BotaoCalculadora symbol="=" color="btn-success"/></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar("0")}>0</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar(".")}>.</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-success" onclick={calcular}>=</button></td>
             </tr>
         </tbody>
     </table>
 </div>
+
+<style>
+    @font-face {
+        font-family: 'font';
+        src: url('../../lib/font.otf');
+    }
+    * {
+        font-family: 'font';
+    }
+    input {
+        width: 9.4rem;
+        height: 2.5rem;
+        margin-bottom: 8%;
+        margin-top: 8%;
+        text-align: center;
+    }
+</style>
