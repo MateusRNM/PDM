accd70dc006f9929c6462851d0302a6f8e668cf7
MateusRNM <mateusmt676@gmail.com>
2025-06-04 11:09:14 -0400

    update

--- src/routes/calculadora/+page.svelte
+++ src/routes/calculadora/+page.svelte
@@ -2,24 +2,74 @@
     let expressao = $state("")
     
     function addChar(char){
+        if(expressao == "ERRO") expressao = ""
         expressao += char
+        formatar()
+    }
+
+    function removeChar(){
+        expressao = expressao.substr(0, expressao.length-1)
+        formatar()
     }
 
     function clear(){
         expressao = ""
     }
 
+    function invertSignal(){
+        let expr = expressao.split('')
+        for(let i = expr.length-1; i >= 0; i--){
+            if(expr[i] == "-"){
+                expr[i] = "+"
+                expressao = expr.join('')
+                formatar()
+                return 
+            } else if(expr[i] == "+"){
+                expr[i] = "-"
+                expressao = expr.join('')
+                formatar()
+                return
+            } else if(expr[i] == "x" || expr[i] == "/"){
+                let expr1 = expr.slice(0, i+1)
+                expr1 += "-"
+                let expr2 = expr.slice(i+1, expr.length)
+                expr1 = expr1.concat(expr2)
+                expressao = expr1.replaceAll(",", "")
+                formatar()
+                return
+            }
+        }
+        expressao = "-" + expr.join('')
+        formatar()
+    }
+
     function calcular(){
+        if(expressao == "") return
+        let expressaoFormatada = expressao.replaceAll('x', '*').replaceAll('%', '/100').replaceAll("^", '**')
+        try {
+            expressao = String(eval(expressaoFormatada))
+        } catch {
+            expressao = "ERRO"
+        }
+    }
+
+    function formatar(){
 
     }
 </script>
 
 <div class="position-absolute start-50 top-50 translate-middle border border-3 border-black p-1 rounded">
 	<div class="container text-center">
-        <input type="text" class="form-control" disabled={true} bind:value={expressao}>
+        <input type="text" class="form-control" readonly bind:value={expressao}>
     </div>
 	<table>
         <tbody>
+            <tr>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-danger" onclick={removeChar}>-</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => invertSignal()}><i class="bi bi-plus-slash-minus"></i></button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("^")}>^</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("%")}>%</button></td>
+            </tr>
             <tr>
                 <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-warning" onclick={clear}>C</button></td>
                 <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("(")}>(</button></td>
@@ -30,7 +80,7 @@
                 <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar("7")}>7</button></td>
                 <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar("8")}>8</button></td>
                 <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar("9")}>9</button></td>
-                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("X")}>X</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("x")}>x</button></td>
             </tr>
             <tr>
                 <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-dark" onclick={() => addChar("4")}>4</button></td>
