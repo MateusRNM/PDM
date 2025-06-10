65db7f3100ee77515c39a17b7aacdd750e9b06ea
MateusRNM <mateusmt676@gmail.com>
2025-06-10 08:26:35 -0400

    commit

--- src/routes/calculadora/+page.svelte
+++ src/routes/calculadora/+page.svelte
@@ -3,6 +3,7 @@
     let expressao = $state("")
     let soundOn = $state(true)
     let sound = null
+    const chars = [".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     
     function addChar(char){
         typeSound()
@@ -40,7 +41,7 @@
                 expr[i] = "-"
                 expressao = expr.join('')
                 return
-            } else if(expr[i] == "x" || expr[i] == "/" || expr[i] == "^" || expr[i] == "%"){
+            } else if(expr[i] == "x" || expr[i] == "/" || expr[i] == "^" || expr[i] == "%" || expr[i] == "("){
                 let expr1 = expr.slice(0, i+1)
                 expr1 += "-"
                 let expr2 = expr.slice(i+1, expr.length)
@@ -52,6 +53,33 @@
         expressao = "-" + expr.join('')
     }
 
+    function arredondar(){
+        typeSound()
+        if(expressao == "ERRO"){
+            expressao = ""
+            return
+        }
+        let expr = expressao.split('')
+        let num = ""
+        for(let i = expr.length-1; i >= 0; i--){
+            if(!chars.includes(expr[i])){
+                let numRounded = String(Math.round(Number(num))).split('')
+                console.log(numRounded)
+                let x = 0
+                for(; i < numRounded.length; x++){
+                    expr[i+x] = numRounded[x]
+                }
+                if (numRounded.length > num.length){
+                    expr.splice(i+x+1, numRounded.length - num.length)
+                }
+                break
+            } else {
+                num += expr[i]
+            }
+        }
+        expressao = expr.join('')
+    }
+
     function calcular(){
         typeSound()
         if(expressao == "ERRO"){
@@ -61,6 +89,7 @@
         if(expressao == "") return
         let expressaoFormatada = expressao.replaceAll('x', '*').replaceAll('%', '/100').replaceAll('^', '**')
         expressaoFormatada = expressaoFormatada.replace(/-(\d+)\*\*(-?\d+)/g, '(-$1)**$2')
+        expressaoFormatada = expressaoFormatada.replace(/√(-?\d+)/g, '($1)**0.5')
         try {
             expressao = String(eval(expressaoFormatada))
         } catch {
@@ -93,12 +122,16 @@
         <tbody>
             <tr>
                 <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-danger" onclick={removeChar}>-</button></td>
-                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => invertSignal()}><i class="bi bi-plus-slash-minus"></i></button></td>
                 <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("^")}>^</button></td>
-                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("%")}>%</button></td>
             </tr>
             <tr>
                 <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-warning" onclick={clear}>C</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => invertSignal()}><i class="bi bi-plus-slash-minus"></i></button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => arredondar()}>≈</button></td>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("√")}>√</button></td>
+            </tr>
+            <tr>
+                <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("%")}>%</button></td>
                 <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("(")}>(</button></td>
                 <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar(")")}>)</button></td>
                 <td><button type="button" class="btn w-100 h-100 border border-3 border-black rounded btn-secondary" onclick={() => addChar("/")}>/</button></td>
@@ -140,7 +173,8 @@
         src: url('../../lib/fonts/Calculator.ttf');
     }
     * {
-        font-family: 'font';
+        font-family: 'displayFont';
+        font-size: 18px;
     }
     input {
         width: 9.4rem;
